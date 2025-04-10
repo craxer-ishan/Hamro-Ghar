4f24c0ef64850ef151be69f157fed5849ed55854
craxer-ishan <np03cs4a220191@heraldcollege.edu.np>
2025-04-10 09:09:04 +0545

    it is commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth.js")
+const listingRoutes = require("./routes/listing.js")
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+/* ROUTES */
+app.use("/auth", authRoutes)
+app.use("/properties", listingRoutes)
+
+/* MONGOOSE SETUP */
+const PORT = 3001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    dbName: "test-pro-db",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((err) => console.log(`${err} did not connect`));
